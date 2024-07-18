3c1e3afb03b979746db985df9e51a4adb4b5b248
Harshrj1502 <harshrj1501@gmail.com>
2024-07-18 22:59:46 +0530

    color changed

--- Components/Links/Navbarlink.jsx
+++ Components/Links/Navbarlink.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-scroll";
-const Navbarlink = ({ link, text }) => {
+const Navbarlink = ({ link, text,color }) => {
   return (
     <Link
       activeClass="active"
@@ -8,7 +8,7 @@ const Navbarlink = ({ link, text }) => {
       spy={true}
       smooth={true}
       duration={800}
-      className="text-xl font-semibold hover:text-white hover:font-bold  transition ease-in-out duration-300 hover:-translate-y-1 hover:scale-110"
+      className={`text-xl font-semibold hover:text-${color} hover:font-bold  transition ease-in-out duration-300 hover:-translate-y-1 hover:scale-110`}
     >
       {text}
       {/* <a
--- Components/Navbar/Navbar.jsx
+++ Components/Navbar/Navbar.jsx
@@ -17,17 +17,19 @@ const Navbar = () => {
         <h1 className="text-3xl font-normal text-primary">DEV</h1>
       </Link>
       <div className="flex  gap-5 cursor-pointer  ">
-        <Navbarlink link={"home"} text={"Home"} />
-        <Navbarlink link={"about"} text={"About"} />
-        <Navbarlink link={"services"} text={"Services"} />
-        <Navbarlink link={"skills"} text={"Skills"} />
-        <Navbarlink link={"work"} text={"Work"} />
+        <Navbarlink link={"home"} text={"Home"} color={"white"} />
+        <Navbarlink link={"about"} text={"About"} color={"white"} />
+        <Navbarlink link={"services"} text={"Services"} color={"white"} />
+        <Navbarlink link={"skills"} text={"Skills"} color={"white"} />
+        <Navbarlink link={"work"} text={"Work"} color={"white"} />
       </div>
-      <Link to="contact"
+      <Link
+        to="contact"
         activeClass="active"
         spy={true}
         smooth={true}
-        duration={800}>
+        duration={800}
+      >
         <Button text={"Contact"} />
       </Link>
     </nav>
