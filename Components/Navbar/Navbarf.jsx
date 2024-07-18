b2e92c790977c670a3df312ae7a61fc05ab8b9ff
Harshrj1502 <harshrj1501@gmail.com>
2024-07-18 22:46:44 +0530

    navbar fixed

--- Components/Navbar/Navbarf.jsx
+++ Components/Navbar/Navbarf.jsx
@@ -1,49 +1,35 @@
 import React from "react";
 import Button from "../Button/Button";
-
+import Navbarlink from "../Links/Navbarlink";
+import { Link } from "react-scroll";
 const Navbar = () => {
   return (
-    <nav className="flex justify-around items-center p-6 ">
-      <div className="flex cursor-pointer">
-        <h1 className="text-xl ">HR</h1>
-
-        <h1 className="text-3xl text-primary">DEV</h1>
+    <nav className="flex justify-around items-center p-3 fixed bg-white w-[100%] outline-none  ">
+      <Link
+        to="home"
+        activeClass="active"
+        spy={true}
+        smooth={true}
+        duration={800}
+        className="flex cursor-pointer"
+      >
+        <h1 className="text-xl font-extralight">HR</h1>
+        <h1 className="text-3xl font-normal text-primary">DEV</h1>
+      </Link>
+      <div className="flex  gap-5 cursor-pointer  ">
+        <Navbarlink link={"home"} text={"Home"} />
+        <Navbarlink link={"about"} text={"About"} />
+        <Navbarlink link={"services"} text={"Services"} />
+        <Navbarlink link={"skills"} text={"Skills"} />
+        <Navbarlink link={"work"} text={"Work"} />
       </div>
-      <div className="">
-        <ul className="flex  gap-5 cursor-pointer relative ">
-          <a
-            href="#about"
-            className="text-xl font-semibold hover:text-white hover:font-bold  transition ease-in-out duration-300 hover:-translate-y-1 hover:scale-110"
-          >
-            Home
-          </a>
-          <a
-            href="#about"
-            className="text-xl font-semibold hover:text-white hover:font-bold  transition ease-in-out duration-300 hover:-translate-y-1 hover:scale-110"
-          >
-            About
-          </a>
-          <a
-            href="#Services"
-            className="text-xl font-semibold hover:text-white hover:font-bold  transition ease-in-out duration-300 hover:-translate-y-1 hover:scale-110"
-          >
-            Services
-          </a>
-          <a
-            href="#Portfolio"
-            className="text-xl font-semibold hover:text-white hover:font-bold  transition ease-in-out duration-300 hover:-translate-y-1 hover:scale-110"
-          >
-            Work
-          </a>
-          {/* <a
-            href="#Portfolio"
-            className="text-xl font-semibold hover:text-white hover:font-bold  transition ease-in-out duration-300 hover:-translate-y-1 hover:scale-110"
-          >
-            Contact
-          </a> */}
-        </ul>
-      </div>
-      <Button text={"Contact"} />
+      <Link to="contact"
+        activeClass="active"
+        spy={true}
+        smooth={true}
+        duration={800}>
+        <Button text={"Contact"} />
+      </Link>
     </nav>
   );
 };
--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import {React,useState,useEffect} from "react";
 import Navbar from "../../Components/Navbar/Navbar";
+import Navbarf from "../../Components/Navbar/Navbarf";
 import Button from "../../Components/Button/Button";
 import Wbutton from "../../Components/Button/Wbutton";
 import { Link } from "react-scroll";
+
 const Home = () => {
+  const [navBackground, setNavBackground] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setNavBackground(true);
+      } else {
+        setNavBackground(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+  });
   return (
     <div className="bg-secondary outline-none" id="home">
       {/* <div className="bg-primary  left-[35vw] top-[-70px] h-[140px] w-[140px] rounded-full absolute"></div> */}
-      <Navbar />
+      {!navBackground ?<Navbar /> : <Navbarf/>}
       <main className="flex">
         <div className="w-[50vw] flex justify-center flex-col px-20 gap-5">
           <div className="container flex flex-col gap-5">
