126098ec95b0de6cd9a6cc885a0cb1074fbebee5
Harshrj1502 <harshrj1501@gmail.com>
2024-07-21 20:02:09 +0530

    files added

--- Components/Footer/Footer.jsx
+++ Components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="flex flex-col items-center justify-center h-[35vw] w-[100vw]">
-        <p className="text-4xl font-normal">Let's work together.</p>
-        <p className="text-4xl font-normal text-grey">Get in touch.</p>
+        <p className="text-4xl font-normal text-center">Let's work together.</p>
+        <p className="text-4xl font-normal text-grey text-center">Get in touch.</p>
       </div>
   )
 }
