f7c10d9cc3f3be0acf8e373b1c07c78cd746c9bc
Harshrj1502 <harshrj1501@gmail.com>
2024-07-21 22:37:27 +0530

    files added

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -8,35 +8,37 @@ import "animate.css/animate.min.css";
 import FadeInUp from "../Components/Fadeinup";
 const Work = () => {
   return (
-    <section className="outline-none h-[100vh] bg-primary w-[100vw]  ">
+    <section className="outline-none h-[100vh] bg-primary w-[100vw]">
       <Navbar />
       <FadeInUp>
-      <div className="h-[90vh] flex flex-col justify-center items-center gap-7 w-[100vw]">
-        <p className="text-7xl font-normal text-center ">
-          Let's craft exceptional digital experiences together.
-        </p>
-        <p className="text-2xl font-normal text-center text-grey w-[100vw]  ">
-          Explore my design services, from user interface and experience to
-          prototyping and testing.
-        </p>
-        <div className="flex gap-5 items-center">
-          <h1 className="text-sm font-normal ">Expertise</h1>
-          <Textcard text={"Web Design"} />
-          <Textcard text={"Web Dev"} />
-          <Textcard text={"App Dev"} />
+        <div className="h-[90vh] flex flex-col justify-center items-center gap-7">
+          <p className="text-7xl font-normal text-center ">
+            Let's craft exceptional digital experiences together.
+          </p>
+          <p className="text-2xl font-normal text-center text-grey   ">
+            Explore my design services, from user interface and experience to
+            prototyping and testing.
+          </p>
+          <div className="flex gap-5 items-center">
+            <h1 className="text-sm font-normal ">Expertise</h1>
+            <Textcard text={"Web Design"} />
+            <Textcard text={"Web Dev"} />
+            <Textcard text={"App Dev"} />
+          </div>
         </div>
-      </div>
-      <div className="grid grid-cols-2 gap-10 place-items-center">
-        <Projectcard />
-        <Projectcard />
-        <Projectcard />
-        <Projectcard />
-        <Projectcard />
-        <Projectcard />
-      </div>
+        <div className="flex justify-center">
+          <div className="grid grid-cols-2 gap-5">
+            <Projectcard />
+            <Projectcard />
+            <Projectcard />
+            <Projectcard />
+            <Projectcard />
+            <Projectcard />
+          </div>
+        </div>
       </FadeInUp>
-      <Footer/>
-      <Bottomlink/>
+      <Footer />
+      <Bottomlink />
     </section>
   );
 };
